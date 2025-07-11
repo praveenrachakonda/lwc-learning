6a0ded8fee9bdee8d82b0cd3a4876090d3f6357c
praveenrachakonda <98563245+praveenrachakonda@users.noreply.github.com>
2025-07-10 22:41:49 -0700

    Add files via upload

---
+++ magicBoxGame/magicBoxGame.js
@@ -0,0 +1,34 @@
+import { LightningElement } from 'lwc';
+
+export default class MagicBoxGame extends LightningElement {
+    boxSize = 100;
+
+    changeColor() {
+        const box = this.template.querySelector('.box');
+        box.style.backgroundColor = this.getRandomColor();
+    }
+
+    growBox() {
+        this.boxSize += 20;
+        const box = this.template.querySelector('.box');
+        box.style.width = `${this.boxSize}px`;
+        box.style.height = `${this.boxSize}px`;
+    }
+
+    hideBox() {
+        const box = this.template.querySelector('.box');
+        box.style.display = 'none';
+    }
+
+    getRandomColor() {
+        const colors = ['lightblue', 'lightgreen', 'yellow', 'orange', 'pink', 'purple'];
+        return colors[Math.floor(Math.random() * colors.length)];
+    }
+    reset(){
+        const box = this.template.querySelector('.box');
+        box.style.width = `${this.boxSize}px`;
+        box.style.height = `${this.boxSize}px`;
+        box.style.backgroundColor = 'lightblue';
+        box.style.display = 'block'; 
+    }
+}
